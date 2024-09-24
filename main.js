c639a66a9867a450431c98aa050455bbcfd7451f
Toni Torredemer <toni.torredemer.argiles@gmail.com>
2024-09-24 16:48:43 +0200

    Add multiple JS scripts into the main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//! 3D-BACKGROUND
+
 /**
  * Constants
  */
@@ -214,3 +216,36 @@ window.onload = function () {
     //Start the initial loop function for the first time
     application.loop();
 };
+
+//! 3D-BACKGROUND
+
+
+
+//! WORD ANIMATED
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector('.word-anime');
+    if (textWrapper) {
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+        anime.timeline({loop: true})
+          .add({
+            targets: '.word-anime .letter',
+            scale: [4,1],
+            opacity: [0,99],
+            translateZ: 0,
+            easing: "easeOutExpo",
+            duration: 800,
+            delay: (el, i) => 70*i
+          }).add({
+            targets: '.word-anime',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+          });
+    }
+});
+
+//! WORD ANIMATED
